49e49e87a33bb512ff10b4d30916bfc5f2fb0f07
Tejo <1103mtejo@apps.ipb.ac.id>
2025-07-24 00:45:39 +0700

    update credentials

--- app.js
+++ app.js
@@ -41,3 +41,5 @@ db.sequelize
     });
 
 
+
+
--- config/googledrive.js
+++ config/googledrive.js
@@ -5,13 +5,10 @@ const path = require("path");
 
 // Autentikasi Google Drive API
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.resolve(process.env.GOOGLE_CREDENTIAL), // File credentials dari Google Cloud
+  keyFile: JSON.parse(process.env.GOOGLE_CREDENTIAL), // File credentials dari Google Cloud
   scopes: ["https://www.googleapis.com/auth/drive.file"], // Akses untuk unggah file
 });
 
-if (!fs.existsSync(process.env.GOOGLE_CREDENTIAL)) {
-    throw new Error("Google Cloud credential file not found!");
-};
 
 const driveService = google.drive({ version: "v3", auth });
 
