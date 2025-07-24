ca57d0c5fec707375bc4ca13b2381c1cd2a1451e
Tejo M <mtejo25@gmail.com>
2025-07-24 12:21:04 +0700

    delete image n file

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(morgan("dev"));
 // app.use(cors());
 app.use(cors({
-       origin: process.env.FRONTEND_URL
+       origin: process.env.FRONTEND_URL,
+       credentials: true,
 }));
 
 
